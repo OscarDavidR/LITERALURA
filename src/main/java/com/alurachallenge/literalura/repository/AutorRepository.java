c892c6a0273ae760da358b9328465065c5bcb11a
Oscar Rodriguez <158114244+OscarDavidR@users.noreply.github.com>
2024-06-10 09:38:23 -0600

    LITERALURA
    Uploaded files

---
+++ src/main/java/com/alurachallenge/literalura/repository/AutorRepository.java
@@ -0,0 +1,20 @@
+package com.alurachallenge.literalura.repository;
+
+import com.alurachallenge.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libros")
+    List<Autor> ListarTodosLosAutores();
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libros WHERE SUBSTRING(a.fechaNacimiento, 1, 4) > :year")
+    List<Autor> ListarAutoresPorAno(String year);
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libros WHERE a.nombre ILIKE %:nombre%")
+    Optional<Autor> AutoresPorNombre(String nombre);
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libros WHERE SUBSTRING(a.fechaFallecimiento, 1, 4) > :year")
+    List<Autor> ListarAutoresPorAnofallecimiento(String year);
+
+}
